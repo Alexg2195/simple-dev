947e8a29bbac426b3add791b03db4139c37bd1fb
Alexg2195 <alexg2195@gmail.com>
2016-10-01 20:45:08 -0500

    incorporated index.js snippet and refactored folder creation

--- bin/simple-dev.js
+++ bin/simple-dev.js
@@ -2,16 +2,19 @@
 'use strict';
 var fs = require('fs');
 var cli = require('cli');
-cli.parse({
-  build: [ 'b', 'Build Starter Files', 'boolean', false ]      // -b, Build Start files
-});
+// cli.parse({
+//   build: [ 'b', 'Build Starter Files', 'boolean', false ]      // -b, Build Start files
+// });
 var options = cli.parse(process.argv);
 
 
-if (options[2] == 'build') {
-  var dir_routes = './routes';
-  var dir_models = './models';
+if (options[2] == 'create') {
   var file_index = './index.js';
+  var dir = [
+    './config',
+    './routes',
+    './models'
+  ];
 
 
   console.log(``);
@@ -19,30 +22,28 @@ if (options[2] == 'build') {
   console.log(``);
 
 
-  if (!fs.existsSync(dir_routes)){
-    fs.mkdirSync(dir_routes);
-    console.log(`++ ${dir_routes} created!`);
-    console.log(``);
-  } else {
-    console.log(`-> ${dir_routes} already created!`);
-    console.log(``);
-  }
-
-  if (!fs.existsSync(dir_models)){
-    fs.mkdirSync(dir_models);
-    console.log(`++ ${dir_models} created!`);
-    console.log(``);
-  } else {
-    console.log(`-> ${dir_models} already created!`);
-    console.log(``);
-  }
-
+  // -----> Create index.js <----------
   if (!fs.existsSync(file_index)){
     fs.openSync(file_index, 'w');
+    fs.writeFileSync(file_index, fs.readFileSync('./bin/snippets/file_index.txt'));
     console.log(`++ ${file_index} created!`);
     console.log(``);
   } else {
     console.log(`-> ${file_index} already created!`);
     console.log(``);
   }
+
+
+  // -----> Create folders <-----------
+  for (var i = 0; i < dir.length; i++) {
+    if (!fs.existsSync(dir[i])){
+      fs.mkdirSync(dir[i]);
+      console.log(`++ ${dir[i]} created!`);
+      console.log(``);
+    } else {
+      console.log(`-> ${dir[i]} already created!`);
+      console.log(``);
+    }
+  }
+
 }
