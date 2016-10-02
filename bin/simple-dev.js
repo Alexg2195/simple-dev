602dc9f92bffef8a6f60468dd6fcee0c0747a77c
Alexg2195 <alexg2195@gmail.com>
2016-10-01 21:03:45 -0500

    added delete method for rapid testing

--- bin/simple-dev.js
+++ bin/simple-dev.js
@@ -8,30 +8,15 @@ var cli = require('cli');
 var options = cli.parse(process.argv);
 
 
-if (options[2] == 'create') {
-  var file_index = './index.js';
-  var dir = [
-    './config',
-    './routes',
-    './models'
-  ];
+if (options[2] == 'create' && options[3] == 'app') {
+  var dir = ['./config', './routes', './models'];
+  var file = ['./index.js', './config/connection.js'];
+  var snippet = ['./bin/snippets/file_index.txt', './bin/snippets/file_connection.txt'];
 
 
   console.log(``);
   console.log(`----> Starting initial build <----`);
   console.log(``);
-
-
-  // -----> Create index.js <----------
-  if (!fs.existsSync(file_index)){
-    fs.openSync(file_index, 'w');
-    fs.writeFileSync(file_index, fs.readFileSync('./bin/snippets/file_index.txt'));
-    console.log(`++ ${file_index} created!`);
-    console.log(``);
-  } else {
-    console.log(`-> ${file_index} already created!`);
-    console.log(``);
-  }
 
 
   // -----> Create folders <-----------
@@ -42,6 +27,62 @@ if (options[2] == 'create') {
       console.log(``);
     } else {
       console.log(`-> ${dir[i]} already created!`);
+      console.log(``);
+    }
+  }
+
+
+  // -----> Create files <-------------
+  for (var i = 0; i < file.length; i++) {
+    if (!fs.existsSync(file[i])){
+      fs.openSync(file[i], 'w');
+      fs.writeFileSync(file[i], fs.readFileSync(snippet[i]));
+      console.log(`++ ${file[i]} created!`);
+      console.log(``);
+    } else {
+      console.log(`-> ${file[i]} already created!`);
+      console.log(``);
+    }
+  }
+}
+
+
+
+
+
+
+// -------------------------> FOR DEVELOPMENT ONLY <-----------------------------
+
+if (options[2] == 'delete') {
+  var dir = ['./config', './routes', './models'];
+  var file = ['./index.js', './config/connection.js'];
+  var snippet = ['./bin/snippets/file_index.txt', './bin/snippets/file_connection.txt'];
+
+
+  console.log(``);
+  console.log(`----> DELETING build <----`);
+  console.log(``);
+
+  // -----> Create files <-------------
+  for (var i = 0; i < file.length; i++) {
+    if (fs.existsSync(file[i])){
+      fs.unlinkSync(file[i], 'w');
+      console.log(`++ ${file[i]} Deleted!`);
+      console.log(``);
+    } else {
+      console.log(`-> ${file[i]} already Deleted!`);
+      console.log(``);
+    }
+  }
+
+  // -----> Create folders <-----------
+  for (var i = 0; i < dir.length; i++) {
+    if (fs.existsSync(dir[i])){
+      fs.rmdirSync(dir[i]);
+      console.log(`++ ${dir[i]} Deleted!`);
+      console.log(``);
+    } else {
+      console.log(`-> ${dir[i]} already Deleted!`);
       console.log(``);
     }
   }
