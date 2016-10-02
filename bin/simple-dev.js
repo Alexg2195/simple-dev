258bd302d8c910aa6f1b8bb91389039103719e83
Alexg2195 <alexg2195@gmail.com>
2016-10-01 20:08:20 -0500

    creating and logging index and folders

--- bin/simple-dev.js
+++ bin/simple-dev.js
@@ -8,18 +8,41 @@ cli.parse({
 var options = cli.parse(process.argv);
 
 
-console.log(options);
-
-
 if (options[2] == 'build') {
   var dir_routes = './routes';
   var dir_models = './models';
+  var file_index = './index.js';
+
+
+  console.log(``);
+  console.log(`----> Starting initial build <----`);
+  console.log(``);
+
 
   if (!fs.existsSync(dir_routes)){
     fs.mkdirSync(dir_routes);
+    console.log(`++ ${dir_routes} created!`);
+    console.log(``);
+  } else {
+    console.log(`-> ${dir_routes} already created!`);
+    console.log(``);
   }
-  
+
   if (!fs.existsSync(dir_models)){
     fs.mkdirSync(dir_models);
+    console.log(`++ ${dir_models} created!`);
+    console.log(``);
+  } else {
+    console.log(`-> ${dir_models} already created!`);
+    console.log(``);
+  }
+
+  if (!fs.existsSync(file_index)){
+    fs.openSync(file_index, 'w');
+    console.log(`++ ${file_index} created!`);
+    console.log(``);
+  } else {
+    console.log(`-> ${file_index} already created!`);
+    console.log(``);
   }
 }
